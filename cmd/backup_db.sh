7813c6e47b814d105bf098190e756400a2bf5d55
marsender <didier.corbiere@opale-concept.com>
2024-10-22 08:48:40 +0200

    Use env file for database backup script REF #1

--- cmd/backup_db.sh
+++ cmd/backup_db.sh
@@ -14,22 +14,39 @@
 
 source "$HOME/.bashrc"
 
+# Get current path and parent path
+backup_path=$(dirname "$0")
+project_path=$(dirname "$backup_path")
+
 #/usr/bin/git -C /opt/git/marsender/directus-project pull
 
+cd $project_path
+db_database_file=.env_db_database
+if [ ! -f $db_database_file ]; then
+	echo 'Missing db database file' $db_database_file
+	exit 0
+fi
+db_user_file=.env_db_user
+if [ ! -f $db_user_file ]; then
+	echo 'Missing db user file' $db_user_file
+	exit 0
+fi
 db_password_file=.env_db_password
 if [ ! -f $db_password_file ]; then
-	echo 'Missing password file' $db_password_file
+	echo 'Missing db password file' $db_password_file
 	exit 0
 fi
 
-db_name=directus-project
-backup_path=/opt/git/marsender/directus-project/cmd
+#/opt/git/marsender/directus-project/cmd
+db_database=`cat $db_database_file`
+db_user=`cat $db_user_file`
 db_password=`cat $db_password_file`
 
-options="--order-by-primary=true --comments=false --ignore-table=$db_name.doctrine_migration_versions --ignore-table=$db_name.messenger_messages"
-/usr/bin/mariadb-dump -udirectus "-p${db_password}" $options $db_name > $backup_path/$db_name.sql
+echo "*** Backup database $db_database ***" 
+options="--order-by-primary=true --comments=false"
+/usr/bin/mariadb-dump -u${db_user} "-p${db_password}" $options $db_database > $backup_path/$db_database.sql
 
-#/usr/bin/git -C /opt/git/marsender/directus-project commit -m "Backup $db_name" $backup_path
+#/usr/bin/git -C /opt/git/marsender/directus-project commit -m "Backup $db_database" $backup_path
 #/usr/bin/git -C /opt/git/marsender/directus-project push
 
 exit 0
